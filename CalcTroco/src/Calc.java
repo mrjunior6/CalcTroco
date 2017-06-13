343c1116553de79082518c994168de2663361806
Junior <mrjunior6@gmail.com>
2017-06-13 13:25:27 -0300

    Test

--- CalcTroco/src/Calc.java
+++ CalcTroco/src/Calc.java
@@ -25,37 +25,30 @@ public class Calc {
 
 			if (inteiro >= 100) {
 				int x = inteiro / 100;
-				inteiroResto = inteiro % 100;
 				inteiro -= (100 * x);
 				mapTroco.put("Notas de R$100, quantidade", x);
-				// System.out.println("Notas de R$100, quantidade: " + x);
 			}
 
 			if (inteiro >= 50) {
 				int x = inteiro / 50;
 				inteiro -= (50 * x);
 				mapTroco.put("Notas de R$50, quantidade", x);
-				// System.out.println("Notas de R$50, quantidade: " + x);
 			}
 
 			if (inteiro >= 20) {
 				int x = inteiro / 20;
 				inteiro -= (20 * x);
 				mapTroco.put("Notas de R$20, quantidade", x);
-				// System.out.println("Notas de R$20, quantidade: " + x);
 			}
 
 			if (inteiro >= 10) {
 				int x = inteiro / 10;
-				inteiroResto = inteiro % 10;
 				inteiro -= (10 * x);
 				mapTroco.put("Notas de R$10, quantidade", x);
-				// System.out.println("Notas de R$10, quantidade: " + x);
 			}
 
 			if (inteiro >= 5) {
 				int x = inteiro / 5;
-
 				inteiro -= (5 * x);
 				mapTroco.put("Notas de R$5, quantidade", x);
 				// System.out.println("Notas de R$5, quantidade: " + x);
@@ -63,41 +56,40 @@ public class Calc {
 
 			if (inteiro < 5 && inteiro >= 1) {
 				int x = inteiro / 1;
-				inteiroResto = inteiro % 1;
 				inteiro -= (1 * x);
 				mapTroco.put("Moedas de R$1, quantidade", x);
-				// System.out.println("Moedas de R$1, quantidade: " + x);
 			}
 
 			while (decimal >= 0.01) {
+				
 
 				if (decimal >= 0.50) {
-					double x = decimal / 0.50;
-					decimal -= (0.50 * x);
+					decimal -= 0.50;
+					int  x = 0;x++;
 					mapTroco.put("Moedas de R$0.50, quantidade", x);
 				}
 
 				if (decimal >= 0.25) {
-					double x = decimal / 0.25;
-					decimal -= (0.25 * x);
+					decimal -= 0.25;
+					int  x = 0;x++;
 					mapTroco.put("Moedas de R$0.25, quantidade", x);
 				}
 
 				if (decimal >= 0.10) {
-					double x = decimal / 0.10;
-					decimal -= (0.10 * x);
+					decimal -= 0.10;
+					int  x = 0;x++;
 					mapTroco.put("Moedas de R$0.10, quantidade", x);
 				}
 
 				if (decimal >= 0.05) {
-					double x = decimal / 0.05;
-					decimal -= (0.05 * x);
+					decimal -= 0.05;
+					int  x = 0;x++;
 					mapTroco.put("Moedas de R$0.05, quantidade", x);
 				}
 
 				if (decimal < 0.05 && decimal >= 0.01) {
-					double x = decimal / 0.01;
-					decimal -= (0.01 * x);
+					decimal -= 0.01;
+					int  x = 0;x++;
 					mapTroco.put("Moedas de R$0.01, quantidade", x);
 				}
 
